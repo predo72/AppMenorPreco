af87b3a09f711ca521b9462c39d6c4f0b4cb697d
Pedro Lucas <pedrobattistella.232@gmail.com>
2024-04-20 14:15:23 -0300

    teste commit proteção branch main

--- menorpreco/lib/data/dummy_listas.dart
+++ menorpreco/lib/data/dummy_listas.dart
@@ -3,12 +3,17 @@ import 'package:menorpreco/models/lista.dart';
 const DUMMY_LISTAS = {
   '1': Lista(
     id: '1',
-    nome: 'Lista Exemplo 1', 
+    nome: 'Lista Exemplo 1',
     finalizada: 0,
   ),
   '2': Lista(
-    id: '2', 
-    nome: 'Lista Exemplo 2', 
+    id: '2',
+    nome: 'Lista Exemplo 2',
     finalizada: 0,
-  )
+  ),
+  '3': Lista(
+    id: '3',
+    nome: 'Lista Exemplo 3',
+    finalizada: 0,
+  ),
 };
